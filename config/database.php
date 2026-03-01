380af717eccc7d6fc61fd3f709e09a547c34c574
Davin <davinipad53@gmail.com>
2026-03-01 11:01:03 +0700

    gas

--- config/database.php
+++ config/database.php
@@ -58,9 +58,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-        ],
+        ],      
 
         'mariadb' => [
             'driver' => 'mariadb',
@@ -78,10 +78,10 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                Pdo\Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'), // <--- INI YANG BENAR
             ]) : [],
         ],
-
+    
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
